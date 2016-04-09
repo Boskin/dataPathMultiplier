ddb998de6d926561e0135aae518c5c895a76a065
unknown <boskin.erkocevic99@yahoo.com>
2016-04-09 13:31:02 -0600

    Added register module

---
+++ register.v
@@ -0,0 +1,15 @@
+module register#(parameter N = 1)(
+    output reg [N - 1:0] q,
+    input [N - 1:0] d,
+    input write,
+    input clr,
+    input clk
+);
+    always@(posedge clk, clr) begin
+        if(clr == 1) begin
+            q <= {N{1'b0}};
+        end else if(write == 1) begin
+            q <= d;
+        end
+    end
+endmodule
