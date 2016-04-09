5a4796e42f5ec8c461f91674be862defd77ab96e
unknown <boskin.erkocevic99@yahoo.com>
2016-04-09 13:40:28 -0600

    Added input clock signal

--- dataPathMultiplier.v
+++ dataPathMultiplier.v
@@ -1,7 +1,8 @@
 module dataPathMultiplier#(parameter N = 4)(
     output [N * 7 / 2 - 1:0] productDisp,
     input [N - 1:0] a,
-    input [N - 1:0] b
+    input [N - 1:0] b,
+    input clk
 );
     
 endmodule
